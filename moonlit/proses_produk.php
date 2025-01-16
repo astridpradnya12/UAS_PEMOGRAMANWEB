f3d0645257daa11f894cff0e1696024f3a79605c
astridpradnya12 <astridpradnya12@gmail.com>
2025-01-16 21:30:34 +0800

    update data proses produk

--- moonlit/proses_produk.php
+++ moonlit/proses_produk.php
@@ -1,15 +1,14 @@
 <?php
 include 'admin_proses.php';
 
-# CREATE
+// CREATE
 if (isset($_POST['create'])) {
-    $id_produk = $_POST['id produk'];
-    $nama_produk = $_POST['nama_produk'];
+    $nama_produk = $_POST['nama_produk']; 
     $deskripsi = $_POST['deskripsi'];
     $qty = $_POST['qty'];
     $harga = $_POST['harga'];
 
-    $sql = "INSERT INTO produk (id_produk,nama_produk, deskripsi, qty, harga ) VALUES ('$id_produk','$nama_pro
+    $sql = "INSERT INTO produk (nama_produk,deskripsi, qty,harga) VALUES ('$nama_produk', '$deskripsi','$qty','$harga')";
     if ($conn->query($sql) === TRUE) {
         header("Location: index.php");
     } else {
@@ -17,27 +16,27 @@ if (isset($_POST['create'])) {
     }
 }
 
-# UPDATE
+// UPDATE
 if (isset($_POST['update'])) {
-    $id_produk = $_POST['id produk'];
+    $id_produk = $_POST['id_produk'];
     $nama_produk = $_POST['nama_produk'];
     $deskripsi = $_POST['deskripsi'];
     $qty = $_POST['qty'];
     $harga = $_POST['harga'];
 
-    $sql = "UPDATE produk SET id_produk='$id_produk', nama_produk='$nama_produk', deskripsi='$deskripsi', qty='$qty', harga='$harga' 
+    $sql = "UPDATE produk SET nama_produk='$nama_produk', deskripsi='$deskripsi', qty='$qty', harga='$harga' WHERE id_produk=$id_produk";
     if ($conn->query($sql) === TRUE) {
-        header("Location: produk.php");
+        header("Location: admin.php");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
 
-# DELETE
+// DELETE
 if (isset($_GET['delete'])) {
     $id = $_GET['delete'];
 
-    $sql = "DELETE FROM users WHERE id=$id";
+    $sql = "DELETE FROM produk WHERE id_produk=$id_produk";
     if ($conn->query($sql) === TRUE) {
         header("Location: produk.php");
     } else {
@@ -45,11 +44,11 @@ if (isset($_GET['delete'])) {
     }
 }
 
-# EDIT
+// EDIT (Untuk mendapatkan data user saat mengedit)
 if (isset($_GET['edit'])) {
     $id = $_GET['edit'];
 
-    $result = $conn->query("SELECT * FROM produk WHERE id=$id");
+    $result = $conn->query("SELECT * FROM produk WHERE id_produk=$id_produk");
     $row = $result->fetch_assoc();
     ?>
     <!DOCTYPE html>
@@ -61,19 +60,19 @@ if (isset($_GET['edit'])) {
     </head>
     <body>
         <h2>Edit Data</h2>
-        <form action="proses_produk.php" method="POST">
+        <form action="proses.php" method="POST">
             <input type="hidden" name="id_produk" value="<?= $row['id_produk'] ?>">
-            <label for="nama">Nama Produk:</label>
+            <label for="nama">nama produk:</label>
             <input type="text" name="nama_produk" value="<?= $row['nama_produk'] ?>" required><br>
-            <label for="nama">Deskripsi:</label>
+            <label for="nama">deskripsi:</label>
             <input type="text" name="deskripsi" value="<?= $row['deskripsi'] ?>" required><br>
-            <label for="email">Qty:</label>
-            <input type="email" name="qty" value="<?= $row['qty'] ?>" required><br>
-            <label for="email">Harga:</label>
+            <label for="email">qty:</label>
+            <input type="text" name="qty" value="<?= $row['qty'] ?>" required><br>
+            <label for="email">harga:</label>
             <input type="text" name="harga" value="<?= $row['harga'] ?>" required><br>
             <button type="submit" name="update">Update</button>
         </form>
-        <a href="produk.php">Kembali</a>
+        <a href="admin.php">Kembali</a>
     </body>
     </html>
     <?php
