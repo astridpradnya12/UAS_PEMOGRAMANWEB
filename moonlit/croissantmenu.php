3ca697f43ee3724f61ee849ae3f639af1886dd75
vanessaputtri <cinemas.vanessa.9c.28@gmail.com>
2025-01-20 14:40:28 +0800

    edit croissant

--- moonlit/croissantmenu.php
+++ moonlit/croissantmenu.php
@@ -33,11 +33,13 @@
         <h2>Chocolate Almond</h2>
         <p>Rp. 25.000</p>
       </div>
+      <button class="add-to-cart-button">Add to Cart</button>
       <div class="product-mac">
         <img src="assets/croMatcha.jpg" alt="Croissant">
         <h2>Matcha</h2>
         <p>Rp. 25.000</p>
       </div>
+      <button class="add-to-cart-button">Add to Cart</button>
       <div class="product-mac">
         <img src="assets/croStrawVanilla.jpeg" alt="Croissant">
         <h2>Strawberry Vaniila</h2>
@@ -48,6 +50,7 @@
         <h2>Vanilla</h2>
         <p>Rp. 25.000</p>
       </div>
+      <button class="add-to-cart-button">Add to Cart</button>
     </div>
   </div>
 <footer>
